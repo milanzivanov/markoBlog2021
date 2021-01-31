2e16703ce6a1de00942d3db8b2e6d4ecc512d605
milan.zivanov <milan.zivanov@gmail.com>
2021-01-31 19:17:02 +0100

    second commit and i am not satisfied 31012021 1

--- src/js/main.js
+++ src/js/main.js
@@ -1,39 +1,37 @@
+let header = document.querySelector('.header');
+
 
 async function fetchData() {
     const responseData = await fetch("https://next.json-generator.com/api/json/get/VyEjrvLyq");
     const data = await responseData.json();
     console.log(data);
-
-    let output = "";
+    
+    let output = '';
 
     for(let d of data) {
 
-
         output += `
-               <h1>
-                    ${d.name}
-                </h1>
-               <p>
-                    ${d.email}
-                <p/>
 
-                <ul>
-                    <li>
-                        <span>city: </span>
-                        ${d.address.city}
-                    </li>
-                    <li><span>street: </span>${d.address.street}</li>
-                    <li><span>zip: </span>${d.address.zipcode}</li>
-                </ul>
+        <h2>
+            ${d.name}
+        </h2>
+        <p>
+            ${d.song}
+        <p/>
 
-                <h2>
-                    <span>${d.address.geo.lat}</span>
-                    <span>${d.address.geo.lng}</span>
-                </h2>
+        <ul>
+            <li>
+                ${d.username}
+            </li>
+            <li>
+                ${d.city}
+            </li>
+            <li>
+                ${d.year}
+            </li>
+        </ul>
+
         `
-
-        
-
 
     }
 
